fbfa8e37451ce11a4f18475b0ea42bd385485ac1
Boutch <niclas13bgmail.com>
2026-04-13 12:51:08 -0400

    Data and welcome modified

---
+++ restaurent code/Script.py
@@ -0,0 +1,3 @@
+from Welcome import *
+
+welcome()
--- restaurent code/Welcome.py
+++ restaurent code/Welcome.py
@@ -1,1 +1,3 @@
-print("test")
+def welcome():
+    print("Welcome to our service of NicoResto!")
+    print("What type of food do you want to eat and press enter if you want to see if its here")
