292e74d4b2bd2f111068a35fb15df311366f0d26
Boutch <niclas13bgmail.com>
2026-04-13 02:04:03 -0400

    message

---
+++ txt.py
@@ -0,0 +1,15 @@
+print("Faites votre choix de votre restautent")
+print("1. McDonald's")
+print("2. KFC")
+print("3. Subway")
+choix = input("Entrez le numéro de votre choix: ")
+if choix == "1":
+    print("Vous avez choisi McDonald's")
+elif choix == "2":
+    print("Vous avez choisi KFC")
+elif choix == "3":
+    print("Vous avez choisi Subway")
+else:
+    print("Choix invalide, veuillez réessayer.")
+
+
